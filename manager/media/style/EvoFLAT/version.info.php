4d3d4173b8a9c57f2c237ed0d18535c3d1f567f9
Nicola <modxit@modx.it>
2019-11-13 20:25:08 +0100

    added version info file

---
+++ manager/media/style/EvoFLAT/version.info.php
@@ -0,0 +1,4 @@
+<?php
+$evoflat_version      = '1.4.10'; // Current version number
+$evoflat_release_date = 'Nov 13, 2019'; // Date of release
+$evo_version          = '1.4.10'; // minimum required evo version
